fe6e247c9336328cb028725091b3108705d33049
Amirabbas Fazelinia <utilyre@gmail.com>
2026-06-12 13:01:17 +0300

    get the structure right

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"bytes"
+	"cmp"
 	"fmt"
 	"html/template"
-	textemplate "text/template"
 	"io"
 	"log"
 	"os"
+	"slices"
+	"strings"
+	textemplate "text/template"
 
 	"github.com/goccy/go-yaml"
 )
@@ -18,18 +21,32 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	journal := groupJournal(content.Journal)
+
+	/* for _, section := range journal {
+		if section.Year == 0 {
+			fmt.Println("n.d.")
+		} else {
+			fmt.Println(section.Year)
+		}
+
+		for _, entry := range section.Entries {
+			fmt.Println("\t", entry.Title)
+		}
+	} */
+
 	Skeleton, err := textemplate.ParseFiles("./templates/Skeleton.html")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	ResourceList, err := template.ParseFiles("./templates/ResourceList.html")
+	Journal, err := template.ParseFiles("./templates/Journal.html")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	var buf bytes.Buffer
-	err = ResourceList.Execute(&buf, content.Resources)
+	err = Journal.Execute(&buf, journal)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -40,17 +57,58 @@ func main() {
 	}
 }
 
-type Content struct {
-	Resources []Resource
+func groupJournal(journal []JournalEntry) []JournalSection {
+	j := slices.SortedFunc(
+		slices.Values(journal),
+		func(a, b JournalEntry) int { return cmp.Compare(b.Year, a.Year) },
+	)
+
+	var sections []JournalSection
+	if len(j) > 0 {
+		curr := JournalSection{
+			Year:    j[0].Year,
+			Entries: []JournalEntry{j[0]},
+		}
+
+		for _, entry := range j[1:] {
+			if entry.Year == curr.Year {
+				curr.Entries = append(curr.Entries, entry)
+			} else {
+				// sort
+				slices.SortFunc(
+					curr.Entries,
+					func(a, b JournalEntry) int { return strings.Compare(a.Title, b.Title) },
+				)
+
+				// collect
+				sections = append(sections, curr)
+
+				// reset
+				curr.Year = entry.Year
+				curr.Entries = []JournalEntry{entry}
+			}
+		}
+
+		sections = append(sections, curr)
+	}
+
+	return sections
 }
 
-type Resource struct {
-	Title        string
-	Associations []string
-	Authors      []string
-	Links        []string
-	Kind         string
-	Tags         []string
+type Content struct {
+	Journal []JournalEntry
+}
+
+type JournalSection struct {
+	Year    uint
+	Entries []JournalEntry
+}
+
+type JournalEntry struct {
+	Title   string
+	Year    uint
+	Authors []string
+	Links   map[string]string
 }
 
 func readContent() (Content, error) {
