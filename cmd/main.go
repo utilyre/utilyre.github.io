fc58f76dd22f8e3e6ee58791b79bb122b456334f
Amirabbas Fazelinia <utilyre@gmail.com>
2026-06-13 21:55:21 +0300

    log stuff out

--- cmd/main.go
+++ cmd/main.go
@@ -12,19 +12,24 @@ func main() {
 	if err != nil {
 		log.Fatalln("open:", err)
 	}
+	log.Println("opened content.yml")
 
 	content, err := ingestion.IngestYAML(f)
 	if err != nil {
 		log.Fatalln("ingest:", err)
 	}
+	log.Println("ingested yaml contents")
 
 	out, err := os.Create("./public/index.html")
 	if err != nil {
 		log.Fatalln("create:", err)
 	}
+	log.Println("created index.html")
+
 
 	err = rendering.RenderContent(out, content)
 	if err != nil {
 		log.Fatalln("render:", err)
 	}
+	log.Println("rendered into index.html")
 }
